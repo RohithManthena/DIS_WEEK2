9aa61f68f099941780a6bd105baa73f65d440059
RohithManthena <smanthena@mail.usf.edu>
2019-09-10 19:40:20 -0400

    Add project files.

---
+++ DIS_WEEK2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DIS_WEEK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            int experience= int.Parse(Console.ReadLine());
+            if (experience == 0)
+            {
+                Console.WriteLine("Need lots of practice");
+            }
+            else if (experience == 1)
+            {
+                Console.WriteLine("looks like you have a little experience");
+            }
+            else if (experience == 2)
+            {
+                Console.WriteLine("wow! you've been doing this for a little while");
+            }
+            else
+            {
+                Console.WriteLine("You are an expert!");
+            }
+ 
+        }
+        }
+ 
+
+    }
